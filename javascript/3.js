03e17dee940ecc6867111aa3107183ae0456cfec
sarthakghimire <sarthakghimire.sg@gmail.com>
2024-12-27 17:25:59 +0545

    test

--- javascript/3.js
+++ javascript/3.js
@@ -14,3 +14,62 @@ if (a % 2 == 0) {
 } else {
   console.log("Tails");
 }
+
+function isEvenOdd(item) {
+  switch (true) {
+    case item % 2 == 0:
+      return "Even";
+      break;
+
+    case item % 2 == 1:
+      return "Odd";
+      break;
+
+    default:
+      return "Invalid Input";
+  }
+}
+
+let result1 = isEvenOdd(4);
+console.log(result1);
+let result2 = isEvenOdd(5);
+console.log(result2);
+let result3 = isEvenOdd("Hi");
+console.log(result3);
+
+switch (new Date().getDay()) {
+  case 0:
+    console.log("Sunday");
+    break;
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("Thursday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+    console.log("Saturday");
+    break;
+  default:
+    console.log("Invalid");
+}
+
+for (let i = 5; i >= 1; i--) {
+  console.log(i);
+}
+
+let info = { name: "ram", age: 23, address: "ktm" };
+for (key12 in info) {
+  console.log(info[key12]);
+}
+
+// for..in loop
