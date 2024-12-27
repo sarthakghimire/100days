7f1686d21a68656c5354a496c38afd6589f78982
sarthakghimire <sarthakghimire.sg@gmail.com>
2024-12-27 16:48:42 +0545

    test

---
+++ javascript/3.js
@@ -0,0 +1,16 @@
+// new Date();
+// console.log(new Date().getDay());
+// console.log(new Date().getHours());
+
+// console.log(Math.E);
+// console.log(Math.trunc(4.56));
+// console.log(Math.floor(4.5));
+// console.log(Math.ceil(4.5));
+// console.log(Math.trunc(Math.random() * 10000));
+
+let a = Math.floor(Math.random() * 100);
+if (a % 2 == 0) {
+  console.log("Heads");
+} else {
+  console.log("Tails");
+}
