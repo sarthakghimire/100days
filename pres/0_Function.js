0621ecf097aa309ea723018bfd347b8a2343a243
sarthakghimire <sarthakghimire.sg@gmail.com>
2025-01-05 21:50:04 +0545

    weather

---
+++ pres/0_Function.js
@@ -0,0 +1,17 @@
+let a = 5;
+let b = 7;
+let c = 34;
+let d = 44;
+
+let sum1 = a + b;
+console.log("The sum is:", sum1);
+
+let sum2 = c + d;
+console.log("The sum is:", sum2);
+
+/*function addNumbers(a, b) {
+  return "The sum is:" + (a + b);
+}
+
+console.log(addNumbers(a, b));
+console.log(addNumbers(c, d));*/
---
+++ pres/1_Scope.js
@@ -0,0 +1,8 @@
+let a = 5;
+
+console.log(a);
+
+function printA() {
+  console.log("The value is:" + a);
+}
+printA();
