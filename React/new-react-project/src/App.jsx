8b75e4d39ed2513271b68eabe1cae00d5a394a9d
sarthakghimire <sarthakghimire.sg@gmail.com>
2025-01-22 17:54:46 +0545

    revise

--- React/new-react-project/src/App.jsx
+++ React/new-react-project/src/App.jsx
@@ -1,18 +1,37 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Blog from "./components/Blog";
 
 const App = () => {
   const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log(count);
+  }, [count]);
 
   return (
     <>
+      <Home />
+      <About />
+      <Contact />
+      <Blog />
       <button
         onClick={() => {
           setCount((prev) => prev + 1);
         }}
       >
-        Click Me
+        Click Me To Increase
       </button>
-      {count}
+      <button
+        onClick={() => {
+          setCount((prev) => prev - 1);
+        }}
+      >
+        Click Me To Decrease
+      </button>
+      <div>Count: {count}</div>
     </>
   );
 };
