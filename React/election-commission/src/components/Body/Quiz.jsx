19e18987c7067c17f0bbe99f5e25eef6f5fe8504
sarthakghimire <sarthakghimire.sg@gmail.com>
2025-01-25 22:32:19 +0545

    election

--- React/election-commission/src/components/Body/Quiz.jsx
+++ React/election-commission/src/components/Body/Quiz.jsx
@@ -1,58 +1,110 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./Quiz.css";
 import data from "./data";
 
 const Quiz = () => {
   let [index, setIndex] = useState(0);
   let [question, setQuestion] = useState(data[index]);
+  let [lock, setLock] = useState(false);
+  let [score, setScore] = useState(0);
+  let [result, setResult] = useState(false);
+
+  let Option1 = useRef(null);
+  let Option2 = useRef(null);
+  let Option3 = useRef(null);
+  let Option4 = useRef(null);
+
+  let option_array = [Option1, Option2, Option3, Option4];
 
   const checkAns = (e, ans) => {
-    if (question.ans == -ans) {
-      e.target.classList.add("correct");
-    } else {
-      e.target.classList.add("wrong onClick={(e)=>{checkAns(e,1)}} ");
+    if (lock === false) {
+      if (question.ans == ans) {
+        e.target.classList.add("correct");
+        setLock(true);
+        setScore((prev) => prev + 1);
+      } else {
+        e.target.classList.add("wrong");
+        setLock(true);
+        option_array[question.ans - 1].current.classList.add("correct");
+      }
+    }
+  };
+
+  const next = () => {
+    if (lock === true) {
+      if (index === data.length - 1) {
+        setResult(true);
+        return 0;
+      }
+
+      setIndex(++index);
+      setQuestion(data[index]);
+      setLock(false);
+      option_array.map((option) => {
+        option.current.classList.remove("correct");
+        option.current.classList.remove("wrong");
+        return null;
+      });
     }
   };
   return (
-    <div style={{ height: "1000px" }}>
+    <div
+      style={{ height: "1000px", display: "flex", justifyContent: "center" }}
+    >
       <div className="quiz-questions">
         <h1>Election Quiz</h1>
         <hr />
+        {result ? (
+          <></>
+        ) : (
+          <>
+            <h2>
+              {index + 1}.{question.question}
+            </h2>
+            <ul>
+              <li
+                ref={Option1}
+                onClick={(e) => {
+                  checkAns(e, 1);
+                }}
+              >
+                {question.option1}
+              </li>
+              <li
+                ref={Option2}
+                onClick={(e) => {
+                  checkAns(e, 2);
+                }}
+              >
+                {question.option2}
+              </li>
+              <li
+                ref={Option3}
+                onClick={(e) => {
+                  checkAns(e, 3);
+                }}
+              >
+                {question.option3}
+              </li>
+              <li
+                ref={Option4}
+                onClick={(e) => {
+                  checkAns(e, 4);
+                }}
+              >
+                {question.option4}
+              </li>
+            </ul>
+            <button onClick={next}>Next Question</button>
+            <p>
+              {index + 1}/{data.length}
+            </p>
+          </>
+        )}
         <h2>
-          {index + 1}.{question.question}
+          Score:{score}/{data.length}
         </h2>
-        <ul>
-          <li
-            onClick={(e) => {
-              checkAns(e, 1);
-            }}
-          >
-            {question.option1}
-          </li>
-          <li
-            onClick={(e) => {
-              checkAns(e, 2);
-            }}
-          >
-            {question.option2}
-          </li>
-          <li
-            onClick={(e) => {
-              checkAns(e, 3);
-            }}
-          >
-            {question.option3}
-          </li>
-          <li
-            onClick={(e) => {
-              checkAns(e, 4);
-            }}
-          >
-            {question.option4}
-          </li>
-        </ul>
-        <button>Next Question</button>
-        <p>1/5</p>
+        <button>Reset</button>
       </div>
     </div>
   );
