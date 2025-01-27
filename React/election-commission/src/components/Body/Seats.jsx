7d8d64db6d6aaf6dc1837b6506a919df7c0bfe33
sarthakghimire <sarthakghimire.sg@gmail.com>
2025-01-27 22:58:33 +0545

    election

--- React/election-commission/src/components/Body/Seats.jsx
+++ React/election-commission/src/components/Body/Seats.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import "./Seats.css";
+import seatsData, { totalSeats } from "./seats";
 
 const Seats = () => {
-  return (
-    <>
-      <div className="for-seat"></div>
-    </>
-  );
+  const createSeats = () => {
+    const elements = [];
+    for (let i = 0; i < totalSeats; i++) {
+      elements.push(
+        <div
+          key={i}
+          style={{
+            width: "10px",
+            height: "10px",
+            backgroundColor: seatsData.color,
+            margin: "2px",
+          }}
+        ></div>
+      );
+    }
+    return elements;
+  };
+
+  return <div id="for-seat">{createSeats()}</div>;
 };
 
 export default Seats;
