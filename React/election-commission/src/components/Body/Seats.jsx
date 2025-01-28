f3cbab0900ea1dfdef582a46160c8ef77aad7746
sarthakghimire <sarthakghimire.sg@gmail.com>
2025-01-28 20:22:52 +0545

    election

--- React/election-commission/src/components/Body/Seats.jsx
+++ React/election-commission/src/components/Body/Seats.jsx
@@ -5,26 +5,46 @@ import Seatbox from "./Seatbox";
 
 const Seats = () => {
   return (
-    <>
+    <div style={{ border: "1px solid black", marginBottom: "2px" }}>
       <h2>Parliament Structure</h2>
-      <div
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-          width: "50%",
-          justifyContent: "center",
-        }}
-      >
-        {seatsData.map((party) =>
-          Array.from({ length: party.seats }).map(() => (
-            <Seatbox
-              // key={`${party.party}-${index}`}
-              backgroundColor={party.color}
-            />
-          ))
-        )}
+      <div style={{ display: "flex" }}>
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            width: "50%",
+            justifyContent: "center",
+          }}
+        >
+          {seatsData.map((party) =>
+            Array.from({ length: party.seats }).map(() => (
+              <Seatbox
+                // key={`${party.party}-${index}`}
+                backgroundColor={party.color}
+              />
+            ))
+          )}
+        </div>
+        <div
+          style={{ border: "1px solid black", width: "250px", padding: "10px" }}
+        >
+          <h3>Legend</h3>
+          {seatsData.map((party, index) => (
+            <div
+              key={index}
+              style={{
+                display: "flex",
+                alignItems: "center",
+                marginBottom: "5px",
+              }}
+            >
+              <Seatbox backgroundColor={party.color} />
+              <span style={{ marginLeft: "10px" }}>{party.party}</span>
+            </div>
+          ))}
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
