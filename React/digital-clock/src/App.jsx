77153ecd5fc06c5c5e789d345b29e359506ae39b
sarthakghimire <sarthakghimire.sg@gmail.com>
2025-02-04 21:22:26 +0545

    clock

---
+++ React/digital-clock/src/App.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from "react";
+
+const App = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+  }, []);
+
+  return (
+    <div className="container">
+      <p>
+        {time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds()}
+      </p>
+    </div>
+  );
+};
+
+export default App;
