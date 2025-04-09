7d3de1d4d8ea0d94c3d8db2819ee3293d7f9da54
sarthakghimire <sarthakghimire.sg@gmail.com>
2025-04-09 16:24:10 +0545

    ?

--- chess/src/Box.jsx
+++ chess/src/Box.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 
-const Box = ({ isDark }) => {
+const Box = ({ isDark, children, onClick }) => {
   const style = {
     width: "60px",
     height: "60px",
     backgroundColor: isDark ? "#769656" : "#eeeed2",
     display: "inline-block",
+    position: "relative",
+    textAlign: "center",
+    verticalAlign: "middle",
+    lineHeight: "60px",
+    fontSize: "32px",
+    cursor: "pointer",
+    userSelect: "none",
   };
 
-  return <div style={style}></div>;
+  return (
+    <div style={style} onClick={onClick}>
+      {children}
+    </div>
+  );
 };
 
 export default Box;
--- chess/src/ChessBoard.jsx
+++ chess/src/ChessBoard.jsx
@@ -1,7 +1,21 @@
+import React, { useState } from "react";
 import Box from "./Box";
-import React from "react";
 
 const ChessBoard = () => {
+  const [rookPosition, setRookPosition] = useState({ row: 0, col: 0 });
+
+  const isLegal = (row, col) => {
+    return row === rookPosition.row || col === rookPosition.col;
+  };
+
+  const checkLegal = (row, col) => {
+    if (isLegal(row, col)) {
+      setRookPosition({ row, col });
+    } else {
+      console.log("Illegal move!");
+    }
+  };
+
   const rows = Array.from({ length: 8 });
   const cols = Array.from({ length: 8 });
 
@@ -11,7 +25,18 @@ const ChessBoard = () => {
         <div key={rowIndex} style={{ display: "flex" }}>
           {cols.map((_, colIndex) => {
             const isDark = (rowIndex + colIndex) % 2 === 1;
-            return <Box key={`${rowIndex}-${colIndex}`} isDark={isDark} />;
+            const hasRook =
+              rowIndex === rookPosition.row && colIndex === rookPosition.col;
+
+            return (
+              <Box
+                key={`${rowIndex}-${colIndex}`}
+                isDark={isDark}
+                onClick={() => checkLegal(rowIndex, colIndex)}
+              >
+                {hasRook && "♖"}
+              </Box>
+            );
           })}
         </div>
       ))}
